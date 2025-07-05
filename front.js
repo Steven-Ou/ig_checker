cebc6134e27e787c808c57305f368a1ab80f9d23
Steven-Ou <osteve425@gmail.com>
2025-07-05 13:32:50 -0400

    adding in import form firebase analytics

--- front.js
+++ front.js
@@ -1,6 +1,7 @@
 //The start of the frontend 
 import React, {useState, useEffect, useCallback} from "react";
 import {initalizeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
 import { getAuth, signInAnonymously, onAuthStateChanged, signInWithCustomToken } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc, collection, getDocs, writeBatch } from 'firebase/firestore';
 import { ArrowUpCircle, UserCheck, UserX, Heart, Shield, Clock, Users, FileUp, BarChart2 } from 'lucide-react';
