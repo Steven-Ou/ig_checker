41f0b99f4a16493278ef798adf1d50a8cc3c9a5f
Steven-Ou <osteve425@gmail.com>
2025-07-05 00:31:05 -0400

    adding in firebase/auth import

--- front.js
+++ front.js
@@ -1,3 +1,4 @@
 //The start of the frontend 
 import React, {useState, useEffect, useCallback} from "react";
 import {initalizeApp} from "firebase/app";
+import { getAuth, signInAnonymously, onAuthStateChanged, signInWithCustomToken } from 'firebase/auth';
