fa11aae534c9d708eead4bc0db926af5826266b6
Steven-Ou <osteve425@gmail.com>
2025-07-05 01:18:03 -0400

    adding imports from lucide-react for the layout

--- front.js
+++ front.js
@@ -3,3 +3,4 @@ import React, {useState, useEffect, useCallback} from "react";
 import {initalizeApp} from "firebase/app";
 import { getAuth, signInAnonymously, onAuthStateChanged, signInWithCustomToken } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc, collection, getDocs, writeBatch } from 'firebase/firestore';
+import { ArrowUpCircle, UserCheck, UserX, Heart, Shield, Clock, Users, FileUp, BarChart2 } from 'lucide-react';
