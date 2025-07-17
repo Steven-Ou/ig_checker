911c6c6c84d3ab90e7edbeb178b64115d89f894f
Steven-Ou <osteve425@gmail.com>
2025-07-16 21:47:34 -0400

    creating the upload List function

--- src/App.js
+++ src/App.js
@@ -195,7 +195,10 @@ const UploadScreen = ({ onUploadComplete}) =>{
             //Use a Firestore "batch" to perform multiple writes as a single atomic operation.
             //This is more efficient and safer than many individual writes.
             const batch = writeBatch(db);
-    
+            
+            const uploadList= aysnc()=>{
+
+            };
         }
     };
 };
