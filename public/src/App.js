0c50a7cd76b23e958373a08e3c57ceb8ae1147ed
Steven-Ou <osteve425@gmail.com>
2025-07-19 22:39:00 -0400

    creating a div with some tails wind

--- public/src/App.js
+++ public/src/App.js
@@ -249,6 +249,8 @@ const UploadScreen = ({ onUploadComplete}) =>{
 
     //The JSX that defines the UI for the upload screen.
     return(
+        <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
 
+        </div>
     );
 };
