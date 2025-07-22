f8f9e40a0d63448c40d5dd30b9a754eae6eab6e1
Steven-Ou <osteve425@gmail.com>
2025-07-22 00:43:34 -0400

    \n

--- public/src/App.js
+++ public/src/App.js
@@ -332,8 +332,8 @@ const Dashboard = ({onSignOut})=>{
             const dataPromises = collections.map(async(colName)=>{
                 const querySnapshot = await getDocs(collection(db, `artifacts/${appId}/users`, uid, colName));
                 return { [colName]: querySnapshot.docs.map(doc => doc.data()) };
-
             });
+            
         }catch(error){
 
         }finally{
