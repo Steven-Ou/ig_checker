c0320db0e87783832abf3744451619e661058227
Steven-Ou <osteve425@gmail.com>
2025-07-21 21:48:54 -0400

    adding SVG

--- public/src/App.js
+++ public/src/App.js
@@ -285,6 +285,8 @@ const UploadScreen = ({ onUploadComplete}) =>{
                 >
                     {isLoading ?(
                         <>
+                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            </svg>
 
                         </>
                     ):'Upload & Analyze'}
