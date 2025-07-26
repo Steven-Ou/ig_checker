af8e729053f0aaf1ee50b1f5b2024be7a3a92ce1
Steven-Ou <osteve425@gmail.com>
2025-07-26 15:28:59 -0400

    creating the nav element

--- public/src/App.js
+++ public/src/App.js
@@ -427,7 +427,9 @@ const Dashboard = ({onSignOut})=>{
             <main className="container mx-auto p-4 sm:p-6 lg:p-8">
                 <div>
                     <aside>
-                        
+                        <nav>
+                            
+                        </nav>
                     </aside>
                 </div>
             </main>
