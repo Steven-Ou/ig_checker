f3c6be0041f3f0bf8cfbd89a70ab8cc03a3eb849
Steven-Ou <osteve425@gmail.com>
2025-07-26 15:28:49 -0400

    creating the aside element

--- public/src/App.js
+++ public/src/App.js
@@ -426,7 +426,9 @@ const Dashboard = ({onSignOut})=>{
             </header>
             <main className="container mx-auto p-4 sm:p-6 lg:p-8">
                 <div>
-                    
+                    <aside>
+                        
+                    </aside>
                 </div>
             </main>
         </div>
