630f762f6ec14c3cdcef8d21f11875da7bce92ee
Steven-Ou <osteve425@gmail.com>
2025-07-29 00:04:41 -0400

    decorating the svg

--- public/src/App.js
+++ public/src/App.js
@@ -545,7 +545,7 @@ export default function App() {
         return(
             <div className="min-h-screen bg-gray-50 flex items-center justify-center">
                 <div className="text-center">
-                    <svg>
+                    <svg className="animate-spin mx-auto h-10 w-10 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle>
 
                         </circle>
