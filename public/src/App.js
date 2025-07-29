02ad22288af1941f927908a558d363d6fb975fff
Steven-Ou <osteve425@gmail.com>
2025-07-29 00:05:17 -0400

    decorating the circle

--- public/src/App.js
+++ public/src/App.js
@@ -546,7 +546,7 @@ export default function App() {
             <div className="min-h-screen bg-gray-50 flex items-center justify-center">
                 <div className="text-center">
                     <svg className="animate-spin mx-auto h-10 w-10 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle>
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4">
 
                         </circle>
                         <path>
